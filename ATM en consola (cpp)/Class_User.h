37eefe03b105d851218eb087ac66fd4d4c5e4169
Lautaro Rojas <lautaro.rojas02@gmail.com>
2022-06-21 20:50:07 -0300

    Update Class_User.h

--- ATM en consola (cpp)/Class_User.h
+++ ATM en consola (cpp)/Class_User.h
@@ -7,3 +7,33 @@ class User
     public: //Funciones
         User(int); //Constructor
 };
+
+class Cliente : public User
+{
+    private: //Atributos
+        
+    public: //Funciones
+        Cliente(int); //Constructor
+        int Retirar_Dinero();
+        int Consultar_Saldo();
+        int Transferir_Dinero();
+};
+
+class Banco : public User
+{
+    private: //Atributos
+        
+    public: //Funciones
+        Banco(int); //Constructor
+        int Transferir_Dinero();
+        int Depositar_Dinero();
+};
+
+class Operador_Banco : public User
+{
+    private: //Atributos
+        
+    public: //Funciones
+        Operador_Banco(int); //Constructor
+        int Depositar_Dinero();
+};
